908dcc3ad875ee9bc1fb74fff2ac9a502ae9f1de
Andres Galeano <afgaleano91@misena.edu.co>
2021-08-16 00:01:36 -0500

    Add new class 'cat'

---
+++ src/main/java/com/platzi/cat_app/Cats.java
@@ -0,0 +1,8 @@
+package com.platzi.cat_app;
+
+public class Cats {
+    int id;
+    String url;
+    String apiKey = "34ed3bec-0d26-490e-8474-3d3403ce86cb";
+
+}
--- src/main/java/com/platzi/cat_app/Main.java
+++ src/main/java/com/platzi/cat_app/Main.java
@@ -1,4 +1,7 @@
 package com.platzi.cat_app;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
