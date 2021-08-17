330ce3d54d7e1a61ad674b51812a783550cd7495
Andres Galeano <afgaleano91@misena.edu.co>
2021-08-16 23:56:43 -0500

    Add New options to menu

--- src/main/java/com/platzi/cat_app/Main.java
+++ src/main/java/com/platzi/cat_app/Main.java
@@ -1,7 +1,39 @@
 package com.platzi.cat_app;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        int optionMenu = -1;
+        String[] buttons = {
+                "1. Ver gatos",
+                "2. Salir"
+        };
 
+        do {
+            //Menu principal
+            String option = (String) JOptionPane.showInputDialog(null,
+                    "Gatitos Java",
+                    "Menu principal",
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    buttons,
+                    buttons[0]);
+            // Validamos que opcion selecciona el usuario
+            for (int i = 0; i < buttons.length; i++) {
+                if(option.equals(buttons[i])){
+                    optionMenu = i;
+                }
+            }
+
+            switch (optionMenu){
+                case 0:
+                    CatsService.seeCats();
+                    break;
+                default:
+                    break;
+            }
+
+        }while (optionMenu !=1);
     }
 }
