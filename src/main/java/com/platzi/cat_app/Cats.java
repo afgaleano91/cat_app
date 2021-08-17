18b3b237eba49c98774782cb3d2e9a696f2ab255
Andres Galeano <afgaleano91@misena.edu.co>
2021-08-16 23:54:52 -0500

    Add new option for query to api cat

--- src/main/java/com/platzi/cat_app/Cats.java
+++ src/main/java/com/platzi/cat_app/Cats.java
@@ -1,8 +1,40 @@
 package com.platzi.cat_app;
 
 public class Cats {
-    int id;
+    String id;
     String url;
     String apiKey = "34ed3bec-0d26-490e-8474-3d3403ce86cb";
+    String image;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
